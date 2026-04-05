16188b8bfdee7eb456ac6edc7bb5bb28fd5c9797
fatihnabani <fadzakin@gmail.com>
2026-04-05 18:41:23 +0700

    add dto User

---
+++ src/dto/User.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict, field_validator, model_validator
+
+
+class UserCreate(BaseModel):
+    first_name: str
+    last_name: str
+    whatsapp: str
+    created_at: datetime
+    updated_at: datetime
+
+    @field_validator("whatsapp")
+    def validate_whatsapp(cls, v):
+        if not v.isdigit():
+            raise ValueError("Nomor whatsapp harus angka")
+        if len(v) < 10:
+            raise ValueError("Nomor whatsapp tidak valid")
+        return v
+
+
+class UserResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    first_name: str
+    last_name: str
+    whatsapp: str
+    created_at: datetime
+    updated_at: datetime
