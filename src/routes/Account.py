4951857ca7e6e22483d73f9b17c80c673a8098a7
fatihnabani <fadzakin@gmail.com>
2026-04-06 08:53:00 +0700

    memperbaiki routes

--- src/routes/Account.py
+++ src/routes/Account.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter
 
-from src.controller.Account import Account
+from src.controller.Account import (
+    create_account,
+    delete_account,
+    get_account,
+    get_accounts,
+    update_account,
+)
 
 router = APIRouter(prefix="/accounts", tags=["Accounts"])
 
 
-router.get("/")(account.get_accounts)
-router.get("/{account_id}")(account.get_account)
-router.post("/")(account.create_account)
-router.put("/{account_id}")(account.update_account)
-router.delete("/{account_id}")(account.delete_account)
+router.get("/")(get_accounts)
+router.get("/{account_id}")(get_account)
+router.post("/")(create_account)
+router.put("/{account_id}")(update_account)
+router.delete("/{account_id}")(delete_account)
--- src/routes/Role.py
+++ src/routes/Role.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter
 
-from src.controller.Role import Role
+from src.controller.Role import (
+    create_role,
+    delete_role,
+    get_role,
+    get_roles,
+    update_role,
+)
 
 router = APIRouter(prefix="/roles", tags=["Roles"])
 
 
-router.get("/")(Role.get_roles)
-router.get("/{role_id}")(Role.get_role)
-router.post("/")(Role.create_role)
-router.put("/{role_id}")(Role.update_role)
-router.delete("/{role_id}")(Role.delete_role)
+router.get("/")(get_roles)
+router.get("/{role_id}")(get_role)
+router.post("/")(create_role)
+router.put("/{role_id}")(update_role)
+router.delete("/{role_id}")(delete_role)
